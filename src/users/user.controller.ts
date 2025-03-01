7b861d48afabd264a96f4e038d4776ab089b1689
Lindsey Dortch <codewithlinds@gmail.com>
2025-02-28 22:17:30 -0600

    Read One for Users

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from 'src/users/user.service';
 import { User as UserModel } from '@prisma/client';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('users/:id')
+  async getUserByID(@Param('id') id: string): Promise<UserModel | null> {
+    return this.userService.user({ id: Number(id) });
+  }
 
   @Get('users')
   async getUsers(): Promise<UserModel[]> {
